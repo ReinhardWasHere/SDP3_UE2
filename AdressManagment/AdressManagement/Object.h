ec0875986a43afe80ac2fe1f6b2daaa8463e53db
Reinhard <reinhard93@gmx.at>
2012-11-04 14:01:44 +0100

    14:01
    Object files from UE01

--- AdressManagment/AdressManagement/Object.h
+++ AdressManagment/AdressManagement/Object.h
@@ -1,11 +1,23 @@
+///////////////////////////////////////////////////////////////////////////
+// Workfile : Object.h
+// Author : Reinhard Penn, Bernhard Selymes
+// Date : 29.10.2012
+// Description : Header for Object.cpp
+// Remarks : -
+// Revision : 0
+///////////////////////////////////////////////////////////////////////////
+
+
 #ifndef OBJECT_H
 #define OBJECT_H
 
 class Object
 {
 public:
+	//virtual Destructor for baseclass
 	virtual ~Object();
 protected:
+	//Default CTor for baseclass
 	Object();
 };
 
